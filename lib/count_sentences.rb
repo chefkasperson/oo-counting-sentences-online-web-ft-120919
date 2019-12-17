a71648901eb6f2a8712e6aff12e522ced53d83e4
chefkasperson <chefkasperson@gmail.com>
2019-12-16 19:56:04 -0800

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    array = self.split(" ")
+    count = 0
+    array.each {|word| 
+      if word[-1] == "."
+        count += 1
+      elsif word[-1] == "!"
+        count += 1
+      elsif word[-1] == "?"
+        count += 1
+      end 
+    }
+    count
   end
+
 end
